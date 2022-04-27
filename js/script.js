eae8e16a25031e59f0d9842f19ee476f391f4b36
Kasia <jan.gawronskakatarzyna04@gmail.com>
2022-04-27 12:02:06 +0200

    Ustalenie ruchu gracza

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var computerMove, randomNumber;
+let computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
@@ -14,3 +14,19 @@ else {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+let playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nozyce.');
+console.log('Wpisana odpowiedz to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} 
+else if (playerInput == '2') {
+    playerMove = 'papier';
+}
+else if (playerInput == '3') {
+    playerMove = 'nozyce';
+}
+else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
